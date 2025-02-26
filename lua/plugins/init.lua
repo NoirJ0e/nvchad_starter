d27fa2d83637711b24609919b63a7eab766e9363
Joe Xu <noirjoe@outlook.com>
2025-02-26 23:23:01 +1100

    add lsp, and copilot-cmp setup

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -17,20 +17,83 @@ return {
     event = "BufEnter"
   },
   {
+    "williaamboman/mason.nvim",
+    opts = {
+      ensure_installed = {
+        "eslint-lsp",
+        "tailwindcss-language-server",
+        "lua-language-server",
+        "typescript-language-server",
+      }
+    }
+  },
+  {
     "zbirenbaum/copilot.lua",
     cmd = "Copilot",
     event = "InsertEnter",
     config = function()
-      require("copilot").setup({})
+      require("copilot").setup {
+        panel = {
+          enabled = true,
+          auto_refresh = true,
+          keymap = {
+            jump_prev = "[[",
+            jump_next = "]]",
+            accept = "<CR>",
+            refresh = "gr",
+            open = "<C-CR>",
+          },
+          layout = {
+            position = "bottom", -- | top | left | right
+            ratio = 0.4,
+          },
+        },
+        suggestion = {
+          enabled = true,
+          auto_trigger = true,
+          debounce = 75,
+          keymap = {
+            accept = "<C-]>",
+            accept_word = "<C-Right>",
+            accept_line = "<C-End>",
+            next = "<M-]>",
+            prev = "<M-[>",
+            dismiss = "<C-[>",
+          },
+        },
+      }
     end,
   },
+
   {
     "zbirenbaum/copilot-cmp",
+    dependencies = { "copilot.lua" },
+    event = { "InsertEnter", "LspAttach" },
+    fix_pairs = true,
     config = function()
       require("copilot_cmp").setup()
-    end
+      -- attach cmp source whenever copilot attaches
+      -- fixes lazy-loading issues with the copilot cmp source
+      vim.api.nvim_create_autocmd("LspAttach", {
+        callback = function(args)
+          local buffer = args.buf ---@type number
+          local client = vim.lsp.get_client_by_id(args.data.client_id)
+          if client and (not "copilot" or client.name == "copilot") then
+            return require("copilot_cmp")._on_insert_enter(client, buffer)
+          end
+        end,
+      })
+      local cmp = require "cmp"
+      local current_sources = cmp.get_config().sources or {}
+      table.insert(current_sources, {
+        name = "copilot",
+        priority = 100,
+      })
+      cmp.setup {
+        sources = current_sources,
+      }
+    end,
   },
-
 
 
   -- {
