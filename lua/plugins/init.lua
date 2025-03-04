8ce4f2f18d54bd20f1edb64b3ae9f6584543bc14
Joe Xu <noirjoe@outlook.com>
2025-03-04 21:17:54 +1100

    Update plugin event and add new provider

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -14,7 +14,7 @@ return {
   },
   {
     "lewis6991/gitsigns.nvim",
-    event = "BufEnter"
+    event = "BufEnter",
   },
   {
     "williaamboman/mason.nvim",
@@ -24,8 +24,8 @@ return {
         "tailwindcss-language-server",
         "lua-language-server",
         "typescript-language-server",
-      }
-    }
+      },
+    },
   },
   {
     "zbirenbaum/copilot.lua",
@@ -99,13 +99,22 @@ return {
     lazy = false,
     version = false, -- Set this to "*" to always pull the latest release version, or set it to false to update to the latest code changes.
     opts = {
-      provider = "openrouter",
+      provider = "gemini_2", -- Replace "openai" with your desired provider
       vendors = {
         openrouter = {
-        __inherited_from = 'openai',
-        endpoint = 'https://openrouter.ai/api/v1',
-        api_key_name = 'OPENROUTER_API_KEY',
-        model = 'anthropic/claude-3.7-sonnet',
+          __inherited_from = "openai",
+          endpoint = "https://openrouter.ai/api/v1",
+          api_key_name = "OPENROUTER_API_KEY",
+          model = "anthropic/claude-3.7-sonnet",
+        },
+        gemini_2 = {
+          __inherited_from = "gemini",
+          endpoint = "https://generativelanguage.googleapis.com/v1beta/models",
+          model = "gemini-2.0-flash",
+          timeout = 30000, -- Timeout in milliseconds
+          api_key_name = "GEMINI_API_KEY",
+          temperature = 0,
+          max_tokens = 8192,
         },
       },
     },
@@ -118,12 +127,12 @@ return {
       "nvim-lua/plenary.nvim",
       "MunifTanjim/nui.nvim",
       --- The below dependencies are optional,
-      "echasnovski/mini.pick",         -- for file_selector provider mini.pick
+      "echasnovski/mini.pick", -- for file_selector provider mini.pick
       "nvim-telescope/telescope.nvim", -- for file_selector provider telescope
-      "hrsh7th/nvim-cmp",              -- autocompletion for avante commands and mentions
-      "ibhagwan/fzf-lua",              -- for file_selector provider fzf
-      "nvim-tree/nvim-web-devicons",   -- or echasnovski/mini.icons
-      "zbirenbaum/copilot.lua",        -- for providers='copilot'
+      "hrsh7th/nvim-cmp", -- autocompletion for avante commands and mentions
+      "ibhagwan/fzf-lua", -- for file_selector provider fzf
+      "nvim-tree/nvim-web-devicons", -- or echasnovski/mini.icons
+      "zbirenbaum/copilot.lua", -- for providers='copilot'
       {
         -- support for image pasting
         "HakonHarnes/img-clip.nvim",
@@ -143,7 +152,7 @@ return {
       },
       {
         -- Make sure to set this up properly if you have lazy=true
-        'MeanderingProgrammer/render-markdown.nvim',
+        "MeanderingProgrammer/render-markdown.nvim",
         opts = {
           file_types = { "markdown", "Avante" },
         },
@@ -152,21 +161,20 @@ return {
     },
   },
   {
-    'sbdchd/neoformat',
-    cmd = 'Neoformat',
+    "sbdchd/neoformat",
+    cmd = "Neoformat",
     config = function()
       -- Create the autocommand group and the BufWritePre autocommand
       vim.api.nvim_create_autocmd("BufWritePre", {
         pattern = "*",
         callback = function()
-          vim.cmd("undojoin | Neoformat")
+          vim.cmd "undojoin | Neoformat"
         end,
         group = vim.api.nvim_create_augroup("fmt", { clear = true }),
       })
     end,
-  }
+  },
 }
-
 
 -- {
 -- 	"nvim-treesitter/nvim-treesitter",
