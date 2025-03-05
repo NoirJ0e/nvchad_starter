8d693f7016162a6ca1179aeb261887a79b36abcc
Joe Xu <noirjoe@outlook.com>
2025-03-06 10:53:45 +1100

    feat: add todo-comments.nvim plugin
    Adds the todo-comments.nvim plugin for enhanced TODO management.

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -4,7 +4,7 @@ require("nvchad.configs.lspconfig").defaults()
 local lspconfig = require "lspconfig"
 
 -- EXAMPLE
-local servers = { "html", "cssls", "pyright", "tailwindcss", "eslint"}
+local servers = { "html", "cssls", "pyright", "tailwindcss", "eslint", "marksman"}
 local nvlsp = require "nvchad.configs.lspconfig"
 
 -- lsps with default config
